eabe258a1b3767960c47c9e474b5654f941b8256
Collins Chirchir <collinskchirchir@gmail.com>
2023-02-22 19:39:54 +0300

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,23 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+end
+
+#READ
+  #.all -> 
+  #.find(1)
+  #.find_by(attribute: "st")
+  # .first, .second, .third, ..., .last
+
+# CREATE
+  # .new()
+  # instance.save
+  # .create()
+
+# UPDATE
+  # .update()
+  # instance.update()
+  # instance.save
+
+# DELETE
+  # instance.destroy
+  # .destroy_all
